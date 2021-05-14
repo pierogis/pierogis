47d5880928b719f14ff538d50c172219dbfbf8c8
Kyle Moore <moore.kyle39@gmail.com>
2021-05-14 09:25:09 -0400

    clean up chef module

--- src/pierogis/chef/__main__.py
+++ src/pierogis/chef/__main__.py
@@ -1,5 +1,6 @@
-import sys
+import os
 import socketserver
+import sys
 from http.server import SimpleHTTPRequestHandler
 
 
@@ -15,9 +16,9 @@ if sys.version_info < (3, 7, 5):
     # Fix for WASM MIME type for older Python versions
     Handler.extensions_map['.wasm'] = 'application/wasm'
 
-
 if __name__ == '__main__':
     port = 8000
+    os.chdir(os.path.dirname(__file__))
     with socketserver.TCPServer(("", port), Handler) as httpd:
         print("Serving at: http://127.0.0.1:{}".format(port))
         httpd.serve_forever()
--- src/pierogis/chef/index.js
+++ src/pierogis/chef/index.js
@@ -1,4 +1,17 @@
-import {initDropArea} from "./input.js";
+import { initDropArea } from "./input.js";
+
+async function loadPyodideAndPackages(){
+    await loadPyodide({ indexURL : "https://cdn.jsdelivr.net/pyodide/v0.17.0/full/" });
+    await pyodide.loadPackage('numpy');
+    await pyodide.loadPackage('pillow');
+    await pyodide.loadPackage('imageio');
+    await pyodide.runPythonAsync(`
+        import micropip
+        await micropip.install('https://files.pythonhosted.org/packages/ad/0c/f7308a3dca51bc2fee95dc8b7f3b6faa52f03b05373a390b302dddaa6aa8/pierogis-0.4.0-cp38-cp38-macosx_11_0_arm64.whl')
+    `)
+}
+
+let pyodideReadyPromise = loadPyodideAndPackages();
 
 async function cook(e) {
     let upperSlider = document.getElementById("upper-slider");
@@ -29,26 +42,17 @@ async function cook(e) {
         }
     }
 
-    globalThis.content = e.target.result;
-    return pyodide.runPythonAsync(`
+    globalThis.input_image_bytes_array = e.target.result;
+
+    return await pyodide.runPythonAsync(`
         import io
         import base64
-        import micropip
-
-        from pyodide import open_url
-        from js import content
-
-        from PIL import Image
-        import numpy as np
-        import imageio
-
-        await micropip.install('https://files.pythonhosted.org/packages/ad/0c/f7308a3dca51bc2fee95dc8b7f3b6faa52f03b05373a390b302dddaa6aa8/pierogis-0.4.0-cp38-cp38-macosx_11_0_arm64.whl')
 
         from pierogis.ingredients import SpatialQuantize, Pierogi, Sort, Threshold, Quantize
 
-        pixels = np.random.randint(0, 255, size=(500, 500, 3), dtype=np.uint8)
+        from js import input_image_bytes_array
 
-        bytes = io.BytesIO(content.to_py())
+        bytes = io.BytesIO(input_image_bytes_array.to_py())
         pierogi = Pierogi.from_path(bytes)
 
         ingredient = ${ingredientString}
@@ -64,31 +68,22 @@ async function cook(e) {
 }
 
 async function cookHandler(){
+    await pyodideReadyPromise;
+
     let imageInput = document.getElementById("image-input");
     var file = imageInput.files[0];
-
-    console.log(file);
 
     const reader = new FileReader();
     reader.onload = async e => {
         cook(e).then(function (imageString) {
             document.getElementById("image").src = imageString;
-        }).catch(function () {
-            console.log("cook failed");
+        }).catch(function (error) {
+            console.log("cook failed: ", error);
         });
     }
 
     reader.readAsArrayBuffer(file);
 }
-
-(async function preload() {
-    await loadPyodide({
-      indexURL : "https://cdn.jsdelivr.net/pyodide/v0.17.0/full/"
-    });
-    await pyodide.loadPackage('numpy');
-    await pyodide.loadPackage('pillow');
-    await pyodide.loadPackage('imageio');
-}())
 
 function init() {
     let upperSlider = document.getElementById("upper-slider");
